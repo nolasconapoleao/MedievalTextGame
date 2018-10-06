764ffafee6e50579e90c52312e50a8205fde7f22
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2018-10-06 15:18:17 +0100

    Player class added
    Defines player controllable class

---
+++ src/main/java/org/gameFreebies/medievalTextGame/resources/characters/Player.java
@@ -0,0 +1,9 @@
+package org.gameFreebies.medievalTextGame.resources.characters;
+
+public class Player extends Human {
+
+
+  public Player(int hp, int dmg, int defense, int magicAttack, int magicDefense) {
+    super(hp, dmg, defense, magicAttack, magicDefense);
+  }
+}
